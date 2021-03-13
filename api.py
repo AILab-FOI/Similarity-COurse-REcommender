fddb4480f0892d840dcebc2f8cc6a131d6713511
Tomislav Peharda <tpeharda@foi.hr>
2021-03-13 10:25:53 +0100

    Cleanup

--- api.py
+++ api.py
@@ -2,9 +2,26 @@ from flask import Flask, request, g
 from input_validation import validator
 from database import query_db, generate_sql_filter
 from data_processing import course_row_to_json, generate_response
-from similarity import compute_similarity
+from similarity import compute_similarity, configure_LSA
 
 app = Flask(__name__)
+
+lsi_model = None
+lsa_index = None
+
+
+def configure_similarity_alg():
+    global lsi_model, lsa_index
+
+    # fetch courses from db
+    courses = query_db(
+        f'SELECT NAME, UNI, COURSE_ID, CREDITS, SEMESTER, DESCRIPTION, GOALS FROM COURSE')
+
+    # format courses data to json
+    courses_json = [course_row_to_json(course) for course in courses]
+
+    # generate lsi model & index
+    lsi_model, lsa_index = configure_LSA(courses_json)
 
 
 @app.route('/check-courses-similarity', methods=['POST'])
@@ -29,7 +46,7 @@ def check_courses_similarity():
 
     # calculate similarity
     courses_json_similarities = compute_similarity(
-        content['input'], courses_json)
+        content['input'], courses_json, lsi_model, lsa_index)
 
     # generate response based on desired fomrat
     output = generate_response(
@@ -47,4 +64,5 @@ def close_connection(exception):
 
 
 if __name__ == '__main__':
+    configure_similarity_alg()
     app.run()
--- similarity.py
+++ similarity.py
@@ -1,11 +1,11 @@
-# from gensim import similarities
-# from gensim import models
-# from gensim import corpora
-# from collections import defaultdict
-# from nltk.corpus import stopwords
-# import numpy as np
-# import nltk
-# nltk.download('stopwords')
+from gensim import similarities
+from gensim import models
+from gensim import corpora
+from collections import defaultdict
+from nltk.corpus import stopwords
+import numpy as np
+import nltk
+nltk.download('stopwords')
 
 
 def to_LSA_comparison_document(description: str, goals: str):
@@ -15,73 +15,76 @@ def to_LSA_comparison_document(description: str, goals: str):
 def generate_similarity_str(similarity):
     return f'{similarity * 100}%'
 
-# def preprocessLSA(documents: list):
-#     stoplist = set(stopwords.words('english'))
 
-#     texts = [
-#             [
-#                 word for word in document.lower().split() if word not in stoplist
-#             ] for document in documents
-#     ]
+def preprocessLSA(documents: list):
+    stoplist = set(stopwords.words('english'))
 
-#     frequency = defaultdict(int)
-#     for text in texts:
-#         for token in text:
-#             frequency[token] += 1
+    texts = [
+            [
+                word for word in document.lower().split() if word not in stoplist
+            ] for document in documents
+    ]
 
-#     texts = [
-#             [
-#                 token for token in text if frequency[token] > 1
-#             ] for text in texts
-#     ]
+    frequency = defaultdict(int)
+    for text in texts:
+        for token in text:
+            frequency[token] += 1
 
-#     dictionary = corpora.Dictionary(texts)
-#     corpus = [dictionary.doc2bow(text) for text in texts]
+    texts = [
+            [
+                token for token in text if frequency[token] > 1
+            ] for text in texts
+    ]
 
-#     return (dictionary, corpus)
+    dictionary = corpora.Dictionary(texts)
+    corpus = [dictionary.doc2bow(text) for text in texts]
 
-
-# def createLSIModel(dictionary, corpus, numTopics: int = 7):
-#     lsi = models.LsiModel(
-#         corpus,
-#         id2word=dictionary,
-#         num_topics=numTopics
-#     )
-
-#     index = similarities.MatrixSimilarity(lsi[corpus])
-
-#     return (lsi, index)
+    return (dictionary, corpus)
 
 
-# def computeLSA(baseDoc: str, documents: list, n: int = 1, lsi: models.LsiModel = None, index=None):
-#     dictionary, corpus = preprocessLSA(documents)
+def createLSIModel(dictionary, corpus, numTopics: int = 7):
+    lsi = models.LsiModel(
+        corpus,
+        id2word=dictionary,
+        num_topics=numTopics
+    )
 
-#     if not lsi:
-#         lsi, index = createLSIModel(dictionary, corpus)
+    index = similarities.MatrixSimilarity(lsi[corpus])
 
-#     if not index:
-#         index = similarities.MatrixSimilarity(lsi[corpus])
-
-#     vecBow = dictionary.doc2bow(baseDoc.lower().split())
-#     vecLSI = lsi[vecBow]
-
-#     sims = index[vecLSI]
-
-#     return sims
+    return (lsi, index)
 
 
-def mock_compute_lsa(baseDoc: str, documents: list):
-    return [x for x in range(len(documents))]
+def configure_LSA(documents: list, lsi: models.LsiModel = None, index=None):
+    dictionary, corpus = preprocessLSA(documents)
+
+    lsi, index = createLSIModel(dictionary, corpus)
+
+    if not index:
+        index = similarities.MatrixSimilarity(lsi[corpus])
+
+    return (lsi, index)
 
 
-def compute_similarity(input_params, courses_json):
+def computeLSA(baseDoc: str, documents: list, lsi: models.LsiModel, index):
+    dictionary, _ = preprocessLSA(documents)
+
+    vecBow = dictionary.doc2bow(baseDoc.lower().split())
+    vecLSI = lsi[vecBow]
+
+    sims = index[vecLSI]
+
+    return sims
+
+
+def compute_similarity(input_params: dict, courses_json: list, lsi_model: models.LsiModel, lsa_index):
     input_document = to_LSA_comparison_document(
         input_params['description'], input_params['goals'])
 
     course_documents = list(map(lambda course: to_LSA_comparison_document(
         course['description'], course['goals']), courses_json))
 
-    similarities = mock_compute_lsa(input_document, course_documents)
+    similarities = computeLSA(
+        input_document, course_documents, lsi_model, lsa_index)
 
     for index, course in enumerate(courses_json):
         course_similarity = similarities[index]
