07382bc7102139a6e861c0e71078bacb2dd8ddae
Tomislav Peharda <tpeharda@foi.hr>
2021-03-22 11:55:47 +0100

    Adjust validation for GET

--- input_validation.py
+++ input_validation.py
@@ -1,28 +1,11 @@
 from cerberus import Validator
 
-input_schema = {
-    'type': 'dict', 'schema': {
-        'description': {"type": 'string', "required": True},
-        'goals': {"type": 'string', "required": True}
-    }
-}
-
-filter_schema = {
-    'type': 'dict', 'schema': {
-        'uni': {'type': 'list', "required": False, 'schema': {'type': 'string'}},
-        'semester': {'type': 'list', "required": False, 'schema': {'type': 'string', 'allowed': ['summer', 'winter']}}
-    }
-}
-
-output_format_schema = {
-    'type': 'string',
-    'allowed': ['json', 'xml', 'csv']
-}
-
 schema = {
-    'input': input_schema,
-    'filter': filter_schema,
-    'outputFormat': output_format_schema,
+    'courseDescription': {"type": 'string', "required": True},
+    'courseGoals': {"type": 'string', "required": True},
+    'filterUni': {'type': 'list', "required": False, 'schema': {'type': 'string'}},
+    'filterSemester': {'type': 'list', "required": False, 'schema': {'type': 'string', 'allowed': ['summer', 'winter']}},
+    'outputFormat': {'type': 'string', 'allowed': ['json', 'xml', 'csv']},
 }
 
 validator = Validator(schema)
